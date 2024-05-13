5f43176f4ecdf2401a8c728a841eb1700050c619
zhangzhen <zhangzhen@tecpie.com>
2024-05-13 15:26:07 +0800

    CaptchaDemo

---
+++ java_diary/src/main/java/io/zeman/captcha/CaptchaDemo.java
@@ -0,0 +1,56 @@
+package io.zeman.captcha;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.CircleCaptcha;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.captcha.ShearCaptcha;
+import cn.hutool.core.lang.Console;
+
+
+
+/**
+ * 验证码测试demo
+ */
+public class CaptchaDemo {
+    public static void main(String[] args) {
+        creatLineCaptcha();
+    }
+
+    /**
+     * 扭曲干扰验证码
+     */
+    private static void creatShearCaptcha() {
+        //定义图形验证码的长、宽、验证码字符数、干扰线宽度
+        ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(200, 100, 4, 4);
+        //ShearCaptcha captcha = new ShearCaptcha(200, 100, 4, 4);
+        //图形验证码写出，可以写出到文件，也可以写出到流
+        captcha.write("d:/shear.png");
+        //验证图形验证码的有效性，返回boolean值
+        boolean verify = captcha.verify("1234");
+        Console.log(verify);
+    }
+
+    /**
+     * 圆圈干扰验证码
+     */
+    private static void creatCircleCaptcha() {
+        //定义图形验证码的长、宽、验证码字符数、干扰元素个数
+        CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 20);
+        //图形验证码写出，可以写出到文件，也可以写出到流
+        captcha.write("d:/circle.png");
+        // 输出code
+        Console.log(captcha.getCode());
+    }
+
+    /**
+     * 线段干扰验证码
+     */
+    private static void creatLineCaptcha() {
+        // 定义图形验证码的长和宽
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100,4,100);
+        // 输出到指定目录
+        lineCaptcha.write("d:/line.png");
+        //输出code
+        Console.log(lineCaptcha.getCode());
+    }
+}
